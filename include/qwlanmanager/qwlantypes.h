639cf09724494b4a104846b4973c78cf61b1567e
Leger Charlie <c.leger@borea-dental.com>
2025-06-20 15:34:25 +0200

    [enums] Fix wrong export of namespace enums

--- include/qwlanmanager/qwlantypes.h
+++ include/qwlanmanager/qwlantypes.h
@@ -1,6 +1,8 @@
 #ifndef QWLANMAN_QWLANTYPES_H
 #define QWLANMAN_QWLANTYPES_H
 
+#include "qwlanman_global.h"
+
 #include <QObject>
 
 /*****************************/
@@ -9,31 +11,31 @@
 
 namespace qwm
 {
-    Q_NAMESPACE
+    Q_NAMESPACE_EXPORT(QWLANMAN_EXPORT)
 
-enum class WlanError
-{
-    WERR_NO_ERROR = 0,
-    WERR_OPERATION_CANCEL,
-    WERR_OPERATION_TIMEOUT,
-    WERR_ITEM_INVALID,
-    WERR_IFACE_BUSY,
-    WERR_NET_UNAVAILABLE,
-    WERR_NET_PASSKEY,
-    WERR_API_INTERNAL
-};
-Q_ENUM_NS(WlanError)
+    enum class WlanError
+    {
+        WERR_NO_ERROR = 0,
+        WERR_OPERATION_CANCEL,
+        WERR_OPERATION_TIMEOUT,
+        WERR_ITEM_INVALID,
+        WERR_IFACE_BUSY,
+        WERR_NET_UNAVAILABLE,
+        WERR_NET_PASSKEY,
+        WERR_API_INTERNAL
+    };
+    Q_ENUM_NS(WlanError)
 
-QString wlanErrorToString(WlanError idErr);
+    QString wlanErrorToString(WlanError idErr);
 
-enum class IfaceState
-{
-    IFACE_STS_IDLE = 0,
-    IFACE_STS_SCANNING
-};
-Q_ENUM_NS(IfaceState);
+    enum class IfaceState
+    {
+        IFACE_STS_IDLE = 0,
+        IFACE_STS_SCANNING
+    };
+    Q_ENUM_NS(IfaceState);
 
-QString ifaceStateToString(IfaceState idState);
+    QString ifaceStateToString(IfaceState idState);
 
 /*****************************/
 /* End namespaces            */
